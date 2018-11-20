29579302c1475678775700a7120305fa8c68869a
Doublc-Qluv <963880465@qq.com>
2018-11-20 16:38:29 +0800

    d

--- Resorce/longintest.py
+++ Resorce/longintest.py
@@ -4,27 +4,13 @@ import sys,os
 # import random
 import socket
 
-#创建服务端的socket对象socketserver
-socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '127.0.0.1'
-
-port =28956
-#绑定地址（包括ip地址会端口号）
-socketserver.bind((host, port))
-#设置监听
-socketserver.listen(5)
-#等待客户端的连接
-#注意：accept()函数会返回一个元组
-#元素1为客户端的socket对象，元素2为客户端的地址(ip地址，端口号)
-clientsocket,addr = socketserver.accept()
-
-
+'''
 def store(user, secret, db):
     md5 = hashlib.md5()
     md5.update(secret.encode('utf-8'))
     result = md5.hexdigest()
     db[user] = result
-
+'''
 
 def verify(db):
     while True:
@@ -44,11 +30,15 @@ def verify(db):
 
 def register(db):
     while True:
-        username = input('输入你的用户名\n')
+        username = name #input('输入你的用户名\n')
+
+        user_file = open('account.txt','r')  # 打开读取用户文件                           #打开帐号文件 
+        user_list = user_file.readlines()   
+
         if username in db.keys():
             print('用户名已存在，请重新输入')
             continue
-        secret = input('输入你的密码:\n')
+        secret = passwd # input('输入你的密码:\n')
         dicta = {'number': 0, 'lower': 0, 'upper': 0, 'other': 0}
         for item in secret:
             if item.isdigit():
@@ -68,19 +58,39 @@ def register(db):
 
 
 if __name__ == '__main__':
+    #创建服务端的socket对象socketserver
+    socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '127.0.0.1'
+
+    port =28956
+    #绑定地址（包括ip地址会端口号）
+    socketserver.bind((host, port))
+    #设置监听
+    socketserver.listen(5)
+    #等待客户端的连接
+    #注意：accept()函数会返回一个元组
+    #元素1为客户端的socket对象，元素2为客户端的地址(ip地址，端口号)
+    clientsocket,addr = socketserver.accept()
+
     while True:
         #接收客户端的请求
         recvmsg = clientsocket.recv(1024)
         #把接收到的数据进行解码
-        strData = eval(recvmsg.decode("utf-8"))    
-            
-        database = {'sam': '67ae79674e56657bb652bd02f7251474'}
-        receive = input('欢迎光临，输入您的身份,新用户（1），老客户（2）,退出（exit）\n')
-        if receive == '1':
-            user, password = register(database)
-            store(user, password, database)
-            print(database)
-        elif receive == '2':
-            verify(database)
-        elif receive == 'exit':
+        strData = eval(recvmsg.decode("utf-8"))
+        
+        head = strData['head']
+        # strData = {'sam': '67ae79674e56657bb652bd02f7251474'}
+        # receive = input()
+        if strData['head'] == '0':
+            print(strData)    
+            name = strData['username']
+            passwd = strData['password']
+
+            user, password = register(strData)
+            # store(user, password, strData)
+            print(strData)
+        elif head == '1':
+            pass#verify(strData)
+        else :
             break
+    socketserver.close()
