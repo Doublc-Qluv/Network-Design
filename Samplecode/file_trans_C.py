d33d66161f25d5c09c0ca258850ad4dabf2bc726
if1997 <1696641280@qq.com>
2018-11-27 15:01:02 +0800

    1

--- Samplecode/file_trans_C.py
+++ Samplecode/file_trans_C.py
@@ -83,7 +83,7 @@ class SOCKET(object):
 
 
 if __name__ == '__main__':
-    c = SOCKET('127.0.0.1',5000)
+    c = socketSOCKET('127.0.0.1',5000)
     
     if hasattr(c,sys.argv[1]):
         func = getattr(c,sys.argv[1])
