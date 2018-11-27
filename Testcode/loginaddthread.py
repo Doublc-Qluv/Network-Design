4bfdcee378d4f19ac4d3c3e95f6797f282a9d87b
Doublc-Qluv <963880465@qq.com>
2018-11-27 19:31:40 +0800

    thread

--- Testcode/loginaddthread.py
+++ Testcode/loginaddthread.py
@@ -5,23 +5,7 @@ import sys,os
 import socket, threading
 import json
 import re
-'''
-def store(user, secret, db):
-    md5 = hashlib.md5()
-    md5.update(secret.encode('utf-8'))
-    result = md5.hexdigest()
-    db[user] = result
-'''
 
-"""
-def send_back(dict):
-    host = '127.0.0.1'
-    port = 28956
-    addr = (host, port)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(addr)
-    s.send(str(dict).encode('utf-8'))
-"""
 def add_onlist(dic,hostport):
     username = dic['username'] #input('输入你的用户名\n')
     # user_file = open('account.txt','r')  # 打开读取用户文件
@@ -72,12 +56,7 @@ def verify(db,hostport):
             dict_passverify['content'] = 'user-or-password-error'
             #send_back(dict_passverify)
             print('user-or-password-error')
-            
-
-               
     return dict_passverify
-
-
 
 def register(db):
     # print(db)    
@@ -119,8 +98,6 @@ def register(db):
                 dicta['upper'] += 1
             else:
                 dicta['other'] += 1
-
-
         if dicta['number'] + dicta['lower'] + dicta['upper'] + dicta['other'] < 4:
             dict_register['Flag'] = 0
             dict_register['content'] = '密码至少大于四位'
@@ -149,6 +126,7 @@ def register(db):
             user_file2.write(jsuser_add)
             user_file2.close()
     return dict_register
+
 def relist_all():
     # 总表
     file = open('Userform', 'r') 
@@ -199,7 +177,31 @@ def community(sockets,useron):
             break
         elif re.match('to: +', datarecv) is not None: #选择通信对象
             pass
-            
+def run(mysocket,addr):
+    recvmsg = mysocket.recv(1024)
+    #把接收到的数据进行解码 
+    dicData = eval(recvmsg.decode('utf-8'))
+    # receive = input()
+    dicData = dict(dicData)
+    print("收到:",dicData)
+    if dicData['Head'] == 'register':
+        print('yes')
+        a=register(dicData)
+        print(a)
+        mysocket.send(str(a).encode('utf-8'))            
+    elif dicData['Head'] == 'login':
+        a=verify(dicData,addr)
+        mysocket.send(str(a).encode('utf-8'))
+        print(a)
+        # 开始工作
+        # run()
+    elif dicData['Head'] == 'UserNameList':
+        a = relist_all()
+        mysocket.send(str(a).encode('utf-8'))
+    else:
+        pass
+    #mysocket.send(str(a).encode('utf-8'))
+
 def start():
     #创建服务端的socket对象socketserver
     socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -208,43 +210,23 @@ def start():
     #绑定地址（包括ip地址会端口号）
     socketserver.bind((host, port))
     #设置监听
-    socketserver.listen(5)
+    socketserver.listen(10)
     #等待客户端的连接
     #注意：accept()函数会返回一个元组
     #元素1为客户端的socket对象，元素2为客户端的地址(ip地址，端口号)
-    #clientsocket,addr = socketserver.accept()
+    #mysocket,addr = socketserver.accept()
     while True:
-        clientsocket,addr = socketserver.accept()
-        #接收客户端的请求s
-        print(clientsocket)
+        mysocket,addr = socketserver.accept()
+        #接收客户端的请求
+        print(mysocket)
         print(addr)
-        recvmsg = clientsocket.recv(1024)
-        #把接收到的数据进行解码 
-        dicData = eval(recvmsg.decode('utf-8'))
-        # dicData = {'sam': '67ae79674e56657bb652bd02f7251474'}
-        # receive = input()
-        dicData = dict(dicData)
-        print("收到:",dicData)
-        if dicData['Head'] == 'register':
-            print('yes')
-            a=register(dicData)
-            print(a)
-            clientsocket.send(str(a).encode('utf-8'))
-            
-        elif dicData['Head'] == 'login':
-            a=verify(dicData,addr)
-            clientsocket.send(str(a).encode('utf-8'))
-            print(a)
-            # 开始工作
-            # run()
-        elif dicData['Head'] == 'UserNameList':
-            a = relist_all()
-            clientsocket.send(str(a).encode('utf-8'))
-        else:
-            pass
-        #clientsocket.send(str(a).encode('utf-8'))
+        t = threading.Thread(target=run, args=(mysocket,addr))
+        t.start()
     socketserver.close()
 
 
+def start_S():
+    s = threading.Thread(target=start)#启用一个线程开启服务器
+    s.start()#开启线程    
 if __name__ == '__main__':
-    start()
+    start_S()
