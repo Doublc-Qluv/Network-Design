c9305d0d6fd94b32fb4158d4c7aeb895d998020e
Doublc-Qluv <963880465@qq.com>
2018-11-28 10:00:12 +0800

    add

---
+++ Testcode/Service_addocm.py
@@ -0,0 +1,253 @@
+import hashlib
+import sys,os
+# from PIL import Image, ImageFont, ImageDraw, ImageFilter
+# import random
+import socket, threading
+import json
+import re
+
+clients = {}    #提供 用户名->socket 映射
+chatwith = {}   #提供通信双方映射
+def add_onlist(dic,hostport):
+    username = dic['username'] #input('输入你的用户名\n')
+    # user_file = open('account.txt','r')  # 打开读取用户文件
+    user_file = open('Usernow', 'r+')
+    # temp_file = open('Usernow','w') # 将在线用户写入一个表
+    jsuser = user_file.read()
+    dict_userold = json.loads(jsuser) # 导入旧表
+    # dict.update(dict2) # 这个函数可以更新字典
+    user_file.close()
+    dict_add = {
+        username:hostport
+        }
+    dict_userold.update(dict_add)
+    jsuser_add = json.dumps(dict_userold)
+    user_file2 = open('Usernow', 'r+') 
+    user_file2.write(jsuser_add)
+    user_file2.close()
+def onlinedict():
+    pass
+    file = open('Usernow', 'r') 
+    js = file.read()
+    dicnow = json.loads(js)
+    file.close()
+    return dicnow
+
+def relist_all():
+    # 总表
+    file = open('Userform', 'r') 
+    js = file.read()
+    dic = json.loads(js)
+    dicn = {}
+    for i in range(len(dic)):
+        dicte = {
+            dic['user'+ str(i)]['name']:str(i)
+        }
+        dicn.update(dicte)
+    L1 = list(dicn.keys())
+    # 在线表
+    file = open('Usernow', 'r') 
+    js = file.read()
+    dicnow = json.loads(js)
+
+    L1 = list(dicn.keys())
+    L2 = list(dicnow.keys())
+    dict_back = {}
+    dict_back['Head'] = 'UserNameList'
+    dict_back['type'] = 'GET'
+    dict_back['ActiveUserList'] = str(L2)
+    dict_back['WholeUserList'] = str(L1)
+    # return L1,L2
+    return dict_back
+
+def verify(db,hostport):
+    print(db)    
+    name = db['username'] 
+    passwd = db['password']
+    # user_file = open('account.txt','r')  # 打开读取用户文件
+    user_file = open('Userform', 'r')
+    # temp_file = open('Usernow','a+') # 将登陆用户写入一个表
+    jsuser = user_file.read()
+    dict_userold = json.loads(jsuser) # 导入旧表
+    dict_passverify = {}
+    dict_passverify['Head'] = 'login'
+    dict_passverify['type'] = 'GET' 
+    print(len(dict_userold))
+    flag = len(dict_userold)
+    for i in range(len(dict_userold)):
+        # print(dict_userold['user'+ str(i)]['name'])
+        if name == dict_userold['user'+ str(i)]['name'] and passwd == dict_userold['user'+ str(i)]['password']:                      
+            if name in onlinedict():
+                flag = 0
+                break  
+            dict_passverify['Flag'] = 1
+            dict_passverify['content'] = 'welcome'
+            #send_back(dict_passverify)
+            print('welcome')
+            add_onlist(db,hostport)
+            break
+        else:
+            flag -= 1
+            continue
+    if flag == 0:
+            dict_passverify['Flag'] = 0
+            dict_passverify['content'] = 'user-or-password-error'
+            #send_back(dict_passverify)
+            print('user-or-password-error')
+    return dict_passverify
+
+def register(db):
+    # print(db)    
+    name = db['username'] 
+    passwd = db['password']
+    username = name #input('输入你的用户名\n')
+    # user_file = open('account.txt','r')  # 打开读取用户文件
+    user_file = open('Userform', 'r')
+    # temp_file = open('Usernow','w') # 将在线用户写入一个表
+    jsuser = user_file.read()
+    dict_userold = json.loads(jsuser) # 导入旧表
+    # dict.update(dict2) # 这个函数可以更新字典
+    user_file.close()
+    dict_register = {}
+    dict_register['Head'] = 'register'
+    dict_register['type'] = 'GET'
+    flag = 0
+    for i in range(len(dict_userold)):
+        if dict_userold['user'+ str(i)]['name'] == username:            
+            dict_register['Flag'] = 0
+            dict_register['content'] = '用户已存在'
+            #send_back(dict_register)
+            print('用户名已存在')
+            break
+        elif dict_userold['user'+ str(len(dict_userold)-1)]['name'] != username:
+            flag = 1
+            break
+        else:
+            continue
+    if flag:
+        secret = passwd # input('输入你的密码:\n')
+        dicta = {'number': 0, 'lower': 0, 'upper': 0, 'other': 0}
+        for item in secret:
+            if item.isdigit():
+                dicta['number'] += 1
+            elif item.islower():
+                dicta['lower'] += 1
+            elif item.isupper():
+                dicta['upper'] += 1
+            else:
+                dicta['other'] += 1
+        if dicta['number'] + dicta['lower'] + dicta['upper'] + dicta['other'] < 4:
+            dict_register['Flag'] = 0
+            dict_register['content'] = '密码至少大于四位'
+            #send_back(dict_register)
+            print('密码至少大于四位')
+        elif dicta['lower'] < 1 or dicta['number'] < 1 : # or dicta['upper'] < 1 or dicta['other'] < 1: 
+            dict_register['Flag'] = 0
+            dict_register['content'] = '密码至少要有一个小写字母以及一个数字'
+            #send_back(dict_register)
+            print('密码至少要有一个小写字母以及一个数字')
+        else:
+            dict_register['Flag'] = 1
+            dict_register['content'] = '注册成功'
+            #send_back(dict_register)
+            print('注册成功')
+            # 存入表单
+            dict_add = {
+                'user'+str(len(dict_userold)):{
+                        'name': username,
+                        'password': secret
+                        }
+                }
+            dict_userold.update(dict_add)
+            jsuser_add = json.dumps(dict_userold)
+            user_file2 = open('Userform', 'w') 
+            user_file2.write(jsuser_add)
+            user_file2.close()
+    return dict_register
+
+
+def community(sockets,useron):
+    pass
+    '''
+    注册后更新表需要客户端发一个总表
+    登陆后发送一个在线列表
+    '''
+    clients = {}    #提供 用户名->socket 映射
+    chatwith = {}   #提供通信双方映射
+    while True:
+        datarecv = sockets.recv(1024)
+        if datarecv == 'quit':  #用户退出# 客户端似乎没写
+            del clients[useron]
+            sockets.send(datarecv.encode('utf-8'))
+            sockets.close()
+            print('%s logout' % useron)
+            break
+        elif re.match('to: +', datarecv) is not None: #选择通信对象
+            pass
+        
+def run(mysocket,addr):
+    while True:
+        recvmsg = mysocket.recv(1024)
+        #把接收到的数据进行解码 
+        dicData = eval(recvmsg.decode('utf-8'))
+        # receive = input()
+        dicData = dict(dicData)
+        print(dicData)
+
+        if dicData['Head'] == 'UserNameList':
+            a = relist_all()
+            mysocket.send(str(a).encode('utf-8'))
+            print(a)
+        elif dicData['Head']=='message':
+
+            print('hello')
+            print(dicData['msg'])
+        else:
+            continue
+        # mysocket.send(str(a).encode('utf-8'))
+
+def start():
+    #创建服务端的socket对象socketserver
+    socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '127.0.0.1'
+    port =28956
+    #绑定地址（包括ip地址会端口号）
+    socketserver.bind((host, port))
+    #设置监听
+    socketserver.listen(10)
+    #等待客户端的连接
+    #注意：accept()函数会返回一个元组
+    #元素1为客户端的socket对象，元素2为客户端的地址(ip地址，端口号)
+    #mysocket,addr = socketserver.accept()
+    while True:
+        mysocket,addr = socketserver.accept()
+        #接收客户端的请求
+
+        recvmsg = mysocket.recv(1024)
+        #把接收到的数据进行解码 
+        dicData = eval(recvmsg.decode('utf-8'))
+
+        if dicData['Head'] == 'register':
+            print('yes')
+            a=register(dicData)
+            print(a)
+            mysocket.send(str(a).encode('utf-8'))            
+        elif dicData['Head'] == 'login':
+            a=verify(dicData,addr)
+            mysocket.send(str(a).encode('utf-8'))
+            print(a)
+            if a['Flag'] == 1:
+                clients['username'] = mysocket
+                t = threading.Thread(target=run, args=(mysocket,addr))
+                t.start()
+        else:
+            pass
+    socketserver.close()
+
+
+def start_S():
+    s = threading.Thread(target=start)#启用一个线程开启服务器
+    s.start()#开启线程
+    #start()    
+if __name__ == '__main__':
+    start_S()
---
+++ Testcode/testlearn.1.py
@@ -0,0 +1,218 @@
+# testlearn
+
+# temptest
+import hashlib
+import sys,os
+# from PIL import Image, ImageFont, ImageDraw, ImageFilter
+# import random
+import socket, threading
+import json
+'''
+def register(db):
+    while True:
+        username = name #input('输入你的用户名\n')
+
+        user_file = open('account.txt','r')  # 打开读取用户文件                           #打开帐号文件 
+        user_list = user_file.readlines()   
+        for user_line in user_list:                                     #对帐号文件进行遍历
+            (user,passwords) = user_line.strip('\n').split()             #分别获取帐号和密码信息
+            if name == user:
+                existence = '用户已存在'
+                dict = {}
+                dict['type'] = 'p'
+                dict['msg'] = '用户已存在'
+                send_back(dict)
+                print('用户名已存在')
+                continue
+
+        secret = passwd # input('输入你的密码:\n')
+        dicta = {'number': 0, 'lower': 0, 'upper': 0, 'other': 0}
+        for item in secret:
+            if item.isdigit():
+                dicta['number'] += 1
+            elif item.islower():
+                dicta['lower'] += 1
+            elif item.isupper():
+                dicta['upper'] += 1
+            else:
+                dicta['other'] += 1
+        if dicta['lower'] < 1: # or dicta['upper'] < 1 or dicta['number'] < 1 or dicta['other'] < 1:
+            dict2 = {}
+            dict2['type'] = 'p'
+            dict2['msg'] = '必须有小写字母'  
+            send_back(dict2)
+            print('密码必须有大、小写字母，数字，和特殊字符四部分组成,请重新输入')
+        else:
+            dict3 = {}
+            dict3['type'] = 'p'
+            dict3['msg'] = '欢迎'
+            send_back(dict3) 
+            print('验证通过，欢迎光临')
+            break
+    return username, secret
+
+if __name__ == '__main__':
+
+    while True:
+
+        head = strData['head']
+        # stData = {'sam': '67ae79674e56657bb652bd02f7251474'}
+        # receive = input()
+        if strData['head'] == '0':
+            print(strData)    
+            name = strData['username'] 
+            passwd = strData['password']
+
+            user, password = register(strData)
+            # store(user, password, strData)
+            print(strData)
+        elif head == '1':
+            pass#verify(strData)
+        else :
+            break
+    socketserver.close()
+
+'''
+
+
+'''
+list_dict_all = []               #创建一个空列表，全局变量，用来存放字典
+def AddtoDict(str_1):            # 定义一个函数，功能：把文件里面的内容添加到字典中
+
+   list_str1 = str_1.split(" ")  # 读取的行内容以字符串的形式显示出来, 使用‘,’分隔字符串
+
+   line_str = []                 # 创建一个空列表，用来接收去掉'\n'的行字符串
+   for i in list_str1:
+       x = i.strip("\n")
+       line_str.append(x)
+   print(line_str)
+
+   dict_all = {}                         # 创建一个空字典
+   for item in line_str:                 # 遍历列表中的行内容，列表中有3个元素
+       if item[0:3] == "url":            # 列表中的元素，前3个字符是否等于“url”
+           dict = {item[0:3]: item[4:]}     # dict = {'url':'http://119.23.241.154:8080/futureloan/mvc/api/member/login'}
+           dict_all.update(dict)            # 添加dict到空字典dict_all中
+           # print(dict_all)
+       else:
+           dict = {item.split(":")[0]: item.split(":")[1]}  # 除url外，取其他数据key, value到字典中
+           dict_all.update(dict)
+   list_dict_all.append(dict_all)                     # 将字典添加到list列表中
+
+def list_dict(file_1):
+    file = open(file_1, "r+")
+    while True:
+        line = file.readline()
+        if line:
+            AddtoDict(line)
+        if not line:
+            break
+    file.close()
+    print(list_dict_all)
+
+list_dict("account.txt")        # 传入文件路径及名称即可实现将文件中的内容以[{}, {}....]的形式输出
+'''
+
+'''
+# 初始化
+dic = {  
+    'user1':{  
+        'name': 'sam',
+        'password': '123' 
+    },  
+    'user0': {  
+        'name': 'root',  
+        'password': 'root'
+    },
+    'user2': {
+        'name': 'kevin',
+        'password': '123'
+    }  
+}   
+js = json.dumps(dic)   
+file = open('Userform', 'w')  
+file.write(js)
+  
+file.close()
+'''
+'''
+# json读取
+username = 'asdd'
+secret = '123'
+file = open('Userform', 'r') 
+js = file.read()
+dic = json.loads(js) 
+dic2 = {
+    'user'+str(len(dic)):{
+                'name': username,
+                'password': secret
+    }
+}
+dic.update(dic2)
+
+for i in range(4):
+    print(dic['user'+str(i)]['name'])
+for i in range(len(dic)):
+    dicnew = {
+        'user'+str(i):dic['user'+str(i)]['name']
+    }
+    dic.update(dicnew)
+print(dic)
+'''
+'''
+# print(dic['user0'].get('name')) # 字典的键找到值
+
+#for x in dic:
+#    print(x)
+
+#while True:
+    #username = name # input('输入你的用户名\n')
+    #secret = passwd # input('输入你的密码:\n')
+
+for n in range(length):
+
+    if username == dic['user'+ str(i)]['name']:
+        if secret == dic['user'+ str(i)]['password']:
+            print('验证通过，欢迎光临')
+            continue                
+        else:
+            print('密码不正确,请重新输入')
+            continue
+    else :
+        print('用户名不正确,请重新输入')
+name = 'username'
+sec = '1234'
+for i in range(len(dic)):
+    if name not in dic['user'+ str(i)]['name']:
+        print('usererror')
+        break
+    if name in dic['user'+ str(i)]['name']:
+        if sec == dic['user'+ str(i)]['password']:
+            print('yes')
+            break
+        else:
+            print('passerror')
+  
+radiansdict.clear()    #删除字典内所有元素
+radiansdict.copy()    #返回一个字典的浅复制
+radiansdict.fromkeys()    #创建一个新字典，以序列seq中元素做字典的键，val为字典所有键对应的初始值
+radiansdict.get(key, default=None)    #返回指定键的值，如果值不在字典中返回default值
+radiansdict.has_key(key)    #如果键在字典dict里返回true，否则返回false
+radiansdict.items()    #以列表返回可遍历的(键, 值) 元组数组
+radiansdict.keys()    #以列表返回一个字典所有的键
+radiansdict.setdefault(key, default=None)    #和get()类似, 但如果键不已经存在于字典中，将会添加键并将值设为default
+radiansdict.update(dict2)    #把字典dict2的键/值对更新到dict里
+radiansdict.values()    #以列表返回字典中的所有值
+'''
+
+def relist_all():
+    pass
+    file = open('Userform', 'r') 
+    js = file.read()
+    dic = json.loads(js)
+    for i in range(len(dic)):
+        dicnew = {
+            'user'+str(i):dic['user'+str(i)]['name']
+        }
+        dic.update(dicnew)
+    return dic
+print(relist_all())
